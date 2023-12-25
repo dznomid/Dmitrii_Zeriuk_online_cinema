8ca017ca892e88f6d6852daf1b75083aebcaff98
dznomid <dznomid@gmail.com>
2023-12-25 19:18:42 +0100

    basic

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dmitrii_Zeriuk_online_cinema"
+include(":app")
+ 
