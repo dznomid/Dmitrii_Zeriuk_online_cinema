95f67dfd68a732ec81ce45ec2f876223f72f0773
dznomid <dznomid@gmail.com>
2024-01-19 20:06:15 +0100

    branch2_2

--- app/src/main/java/com/jimmyeatworm/dmitrii_zeriuk_online_cinema/MainActivity.kt
+++ app/src/main/java/com/jimmyeatworm/dmitrii_zeriuk_online_cinema/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jimmyeatworm.dmitrii_zeriuk_online_cinema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onClickToast(view : View){
+        val buttonText : String = (view as Button).text.toString()
+        Toast.makeText(this, buttonText, Toast.LENGTH_SHORT).show()
     }
 }
