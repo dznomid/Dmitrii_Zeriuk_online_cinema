eef6e381578f744dc5d8a287602f86567b9d36d3
dznomid <dznomid@gmail.com>
2024-01-29 18:06:46 +0100

    backup

--- app/src/main/java/com/jimmyeatworm/dmitrii_zeriuk_online_cinema/MainActivity.kt
+++ app/src/main/java/com/jimmyeatworm/dmitrii_zeriuk_online_cinema/MainActivity.kt
@@ -7,14 +7,50 @@ import com.google.android.material.appbar.AppBarLayout
 
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
     fun onClickToast(view : View){
         val buttonText : String = (view as Button).text.toString()
         Toast.makeText(this, buttonText, Toast.LENGTH_SHORT).show()
     }
+
+
 }
